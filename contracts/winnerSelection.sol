0358a2b6e74c5c8026f58e4bdd0704a18dec817b
yuli <102002128+yuli-iza@users.noreply.github.com>
2024-04-11 00:16:12 -0700

    Update winnerSelection.sol

--- contracts/winnerSelection.sol
+++ contracts/winnerSelection.sol
@@ -8,23 +8,22 @@ contract bettingGame {
 
     // Assign winner (address) and payout in function
     function pseudoRandomWinner() external  {
-        //If payment from both player 1 and 2 is not true then terminate
-        //Cases: bets werent properly placed, or player did not have sufficient balance 
+        // If payment from both player 1 and 2 is not true then terminate
+        // Cases: bets werent properly placed, or player did not have sufficient balance 
         require(paymentPlayer1 && paymentPlayer2, "Bets have not been placed");
         
         // Use hash of the transaction sender as a pseudo-random factor
         bytes32 randNumHash = keccak256(abi.encodePacked(msg.sender, block.timestamp));
         
-        //No implementation in the case of a tie
+        // No implementation in the case of a tie, only one winner selected
         if (uint(randNumHash) % 2 == 0) {
             winner = player1;
         } else {
             winner = player2;
         }
 
-        // Once winner is determined, payout
+        // Once winner is determined payout
         address winnerAddress = winner;
         winnerAddress.transfer(address(this).balance);
     }
-
 }
